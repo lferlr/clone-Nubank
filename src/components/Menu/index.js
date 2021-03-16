c15329811b4a1c75d69b9f62a3eb3d896b70d818
Lucas Rodrigues <rodrigues14set@hotmail.com>
2021-03-16 14:47:10 -0300

    Finished animation and app

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,9 +5,15 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Code, Title, Nav, NavItem, NavText, SignOutButton, SignOutButtonText } from './styles';
 
-export default function Menu() {
+export default function Menu({ translateY }) {
   return(
-    <Container>
+    <Container style={{
+      opacity: translateY.interpolate({
+        inputRange: [0, 150],
+        outputRange: [0, 1],
+      }),
+    }}
+    >
       <Code>
         <Title>QRCode</Title>
         {/* <QRCode
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
 import Menu from '~/components/Menu';
@@ -10,32 +13,94 @@ import {
 } from './styles';
 
 export default function Main() {
+  let offset = 0;
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: { 
+          translationY: translateY,
+        },
+      },
+    ],
+    { useNativeDriver: true },
+  )
+  
+  function onHandlerStateChanged(event) {
+    if (event.nativeEvent.oldfState == State.ACTIVE) {
+      let opened = false;
+      const { translationY } = event.nativeEvent;
+
+      offset += translationY;
+      
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+        offset = 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = Opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      })
+
+     
+    }
+  }
+  
   return (
     <Container>
       <Header />
 
       <Content>
-        <Menu/>
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
+        <Menu translateY={translateY} />
 
-          <CardContent>
-            <Title>Saldo disponível</Title>
-            <Description>R$ 764.549,91</Description>
-          </CardContent>
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChanged}
+        >
+          <Card 
+            style={
+              { 
+                transform: [{
+                  translateY: translateY.interpolate({
+                    inputRange: [0, 340],
+                    outputRange: [0, 340],
+                    extrapolate: 'clamp',
+                  }),
+                }]
+              }
+            }
+          >
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
 
-          <CardFooter>
-            <Annotation>
-              Transferência de R$ 500,00 recebida de Marcos Veloso hoje ás 18:30h
-            </Annotation>
-          </CardFooter>
-        </Card>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 764.549,91</Description>
+            </CardContent>
+
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 500,00 recebida de Marcos Veloso hoje ás 18:30h
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
+
       </Content>
 
-      <Tabs />
+      <Tabs translateY={translateY} />
     </Container>
   );
 }
